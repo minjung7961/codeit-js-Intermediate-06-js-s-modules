6ac4ad5028b4b62bc315bd430913398dbcf940ad
minjung <alswjd7961@naver.com>
2022-03-19 22:31:14 +0900

    another way to use default export

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // 한꺼번에 다루기
 import * as printerjs from './printer.js';
-import { 
-  default as codeit,
+import codeit,{ 
   title, data as members} from './members.js';
 // const title = 'title';
 print(printerjs.title);
