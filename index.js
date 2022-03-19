10aec1e3ba007e58b0b7cc55b40c3dd512817f29
minjung <alswjd7961@naver.com>
2022-03-19 18:42:38 +0900

    Module usage example

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // 값 가져오기
 import {title as printerTitle, print} from './printer.js';
-const title = 'title';
+import {title, data as members} from './members.js';
+// const title = 'title';
 print(title);
+print(printerTitle);
+print(members)
---
+++ members.js
@@ -0,0 +1,6 @@
+export const title = 'CodeitMembers';
+
+export const data = [
+  '영훈','윤수','동욱','영준','규식','태호',
+  '지웅','효신','효준','현승','재하','유니'
+];
