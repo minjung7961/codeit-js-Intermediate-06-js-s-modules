feb21db35f89f0e3888ce527a8dc8a0f56678a31
minjung <alswjd7961@naver.com>
2022-03-19 22:24:42 +0900

    how to use defualt commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // 한꺼번에 다루기
 import * as printerjs from './printer.js';
-import {title, data as members} from './members.js';
+import { 
+  default as codeit,
+  title, data as members} from './members.js';
 // const title = 'title';
 print(printerjs.title);
 print(printerTitle);
